ad5baecd7e2740d42ba60b75cbc457f631c4993f
Lucas <lucas.delima4@gmail.com>
2022-11-13 14:50:47 -0300

    Exercicio 2 completo e Exercicio 1 ainda incompleto

---
+++ ExerciciosSemana1BackEnd/Exercicio1/Exercicio1.cs
@@ -0,0 +1,58 @@
+﻿namespace BackEnd.Semana1.Exercicio1
+{
+    [Serializable]
+    class InvalidPiramideValueException : Exception
+    {
+        public InvalidPiramideValueException() { }
+
+        public InvalidPiramideValueException(int N)
+            : base(string.Format($"Valor inválido de N (N = {N})"))
+        {
+
+        }
+    }
+    public class Piramide
+    {
+        int N;
+
+        public Piramide(int _N)
+        {
+            try
+            {
+                N = _N;
+
+                ValidaN(_N);
+            }
+            catch (InvalidPiramideValueException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public int GetN()
+        {
+            return N;
+        }
+        private static void ValidaN(int _N)
+        {
+            if (_N < 1)
+                throw new InvalidPiramideValueException(_N);
+        }
+        public void Desenha()
+        {
+            int[] values = new int[N - 1];
+
+            //for(int i = 0; i <)
+
+        }
+
+    }
+    class FazPiramide
+    {
+        public static void Main(string[] args)
+        {
+            Piramide p = new(0);
+            Console.WriteLine(p.GetN());
+        }
+    }
+}
---
+++ ExerciciosSemana1BackEnd/Exercicio2/Exercicio2.cs
@@ -0,0 +1,71 @@
+﻿namespace BackEnd.Semana1.Exercicio2
+{
+    public class Vertice
+    {
+        double x, y;
+
+        public Vertice(double _x, double _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        public double GetX()
+        {
+            return x;
+        }
+
+        public double GetY()
+        {
+            return y;
+        }
+        private void SetX(double _x)
+        {
+            x = _x;
+        }
+        private void SetY(double _y)
+        {
+            y = _y;
+        }
+
+        public void Distancia(Vertice _vertice)
+        {
+            double distancia = Math.Sqrt((Math.Pow(_vertice.GetX() - x, 2) + Math.Pow(_vertice.GetY() - y, 2)));
+            Console.WriteLine($"A distância euclidiana entre ({_vertice.GetX()}, {_vertice.GetY()}) e ({x}, {y}) é: {distancia:0.00}");
+        }
+        public void Move(double _x, double _y)
+        {
+            double auxX = x;
+            double auxY = y;
+
+            SetX(_x);
+            SetY(_y);
+
+            Console.WriteLine($"O Vertice era ({auxX}, {auxY}) e mudou para ({x}, {y})");
+
+        }
+        public void TestaVertice(Vertice _vertice)
+        {
+            if (_vertice.GetX() == x && _vertice.GetY() == y)
+            {
+                Console.WriteLine($"O Vertice ({_vertice.GetX()}, {_vertice.GetY()}) é igual ao ({x}, {y})");
+                return;
+            }
+            Console.WriteLine($"O Vertice ({_vertice.GetX()}, {_vertice.GetY()}) não é igual ao ({x}, {y})");
+        }
+    }
+    class FazPiramide
+    {
+        public static void Main(string[] args)
+        {
+            Vertice v1 = new(2.2, 8.1);
+            Vertice v2 = new(2.1, 3.2);
+            Vertice v3 = new(2.1, 3.2);
+
+            v1.Distancia(v2);
+            v1.Move(v2.GetX(), v2.GetY());
+            v2.TestaVertice(v3);
+        }
+    }
+
+
+}
